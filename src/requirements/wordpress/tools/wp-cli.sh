0a39cdc4af05929deb2a647c378e09f1eb048de9
ncampbel <ncampbel@Inception.myguest.virtualbox.org>
2026-02-23 20:11:35 +0000

    Inception: finalizing wordpress container configuration

--- src/requirements/wordpress/tools/wp-cli.sh
+++ src/requirements/wordpress/tools/wp-cli.sh
@@ -6,14 +6,19 @@ set -e
 DB_USER_PASSWORD=$(cat /run/secrets/db_user_password)
 WP_ADMIN_PASSWORD=$(cat /run/secrets/wp_admin_password)
 WP_ADMIN_EMAIL=$(cat /run/secrets/wp_admin_email)
-WP_USER_PASSWORD=$(cat /run/secrets/wp_admin_password)
+WP_USER_PASSWORD=$(cat /run/secrets/wp_user_password)
 WP_USER_EMAIL=$(cat /run/secrets/wp_user_email)
 
-cd /var/www/html
-
-# verify/test if wp-config.php file exists
-# if not, create it, else just run php-fpm
-if [ ! -f wp-config.php ]; then
+# echo "Waiting for MariaDb"
+# until mysql -h mariadb -u "${MYSQL_USER}" -p "${DB_USER_PASSWORD}" SELECT "1"; do
+# echo "MariaDb not ready yet";
+# sleep 2;
+# done
+echo "Waiting for MariaDB TCP port..."
+until nc -z "$DB_HOST" 3306; do
+	sleep 1
+done
+echo "MariaDB is ready!"
 
 # It's important to add --allow root because by default 
 # running wp-cli ad root can be dangerous, because if any code is malicious
@@ -22,33 +27,52 @@ if [ ! -f wp-config.php ]; then
 # to give full acess and install the wp properly
 wp core download --allow-root
 
+cd /var/www/html
+
 # creates a configuration file with the db created by mariadb container
-wp config create \
-    --dbname=$(MYSQL_DATABASE) \
-    --dbuser=$(MYSQL_USER) \
-    --dbpass=$(DB_USER_PASSWORD) \
-    --dbhost=$(DB_HOST) \
-    --locale=en_US \
-    --allow-root
-
-# install wordpress through github repo
-wp core install \
-    --url=$(WP_URL) \
-    --title=$(WP_TITLE) \
-    --admin_user=$(WP_ADMIN_USER) \
-    --admin_password=$(WP_ADMIN_PASSWORD) \
-    --admin_email=$(WP_ADMIN_EMAIL) \
-    --skip-email \
-    --allow-root
-
-# create a new user
-wp user create \
-    --user=$(WP_USER) \
-    --email=$(WP_USER_EMAIL) \
-    --user_pass=$(WP_USER_PASSWORD) \
-    --role=subscriber \
-    --allow-root
+if [ ! -f wp-config.php ]; then
+    echo "wp-config.php does not exist...\n creating configuration file"
+    wp config create \
+        --dbname="${MYSQL_DATABASE}" \
+        --dbuser="${MYSQL_USER}" \
+        --dbpass="${DB_USER_PASSWORD}" \
+        --dbhost="${DB_HOST}" \
+        --locale=en_US \
+        --allow-root
+else
+    echo "wp-config.php exists...\n making sure it has the right configs"
+    wp config set DB_NAME "${MYSQL_DATABASE}" --raw --allow-root
+    wp config set DB_USER "${MYSQL_USER}" --raw --allow-root
+    wp config set DB_PASSWORD "${DB_USER_PASSWORD}" --raw --allow-root
+    wp config set DB_HOST "${DB_HOST}" --raw --allow-root
 
 fi
 
-exec php-fpm
+    if ! wp core is-installed --allow-root; then
+        # install wordpress through github repo
+        echo installing wordpress through github repo
+        wp core install \
+            --url="${WP_URL}" \
+            --title="${WP_TITLE}" \
+            --admin_user="${WP_ADMIN_USER}" \
+            --admin_email="${WP_ADMIN_EMAIL}" \
+            --admin_password="${WP_ADMIN_PASSWORD}" \
+            --skip-email \
+            --allow-root
+    else
+        if wp user get "${WP_ADMIN_USER}" --field=ID --allow-root; then
+            echo updating admin user
+            wp user update "${WP_ADMIN_USER}" \
+                --user_pass="${WP_ADMIN_PASSWORD}" \
+                --role=administrator \
+                --allow-root || true
+        else
+            # create a new user
+            echo create a new user
+            wp user create \
+                --user_pass="${WP_ADMIN_PASSWORD}" \
+                --role=administrator \
+                --allow-root || true
+        fi
+    fi
+exec php-fpm8.2 -F
